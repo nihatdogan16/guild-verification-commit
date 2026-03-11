b89eaa465412a4cf052e16f9bf1949f84cdbef6f
nihatdogan16 <166499856+nihatdogan16@users.noreply.github.com>
2026-03-11 15:59:51 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:59:37.399Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-11T12:59:51.867Z
+Commit: Fix broken links in documentation
