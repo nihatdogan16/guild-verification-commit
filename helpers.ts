becf30e383442c4690ef58f57cb967b8f54da5a4
nihatdogan16 <166499856+nihatdogan16@users.noreply.github.com>
2026-03-11 16:14:49 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:59:45.990Z
-Commit: Add dark mode support
+Timestamp: 2026-03-11T13:14:50.125Z
+Commit: Resolve null pointer exception in user service
